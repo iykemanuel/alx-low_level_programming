ca47282d6bf178fc64d54b3c6c5b20c2dcaee2d8
Iykemanuel <mycreativeplots@gmail.com>
2022-03-10 09:49:06 -0800

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,14 +12,11 @@ int main(void)
 	float afloat;
 	long along;
 	long long alonglong;
-	double adouble;
 
 	printf("Size of an int: %lu byte(s)\n", sizeof(aninteger));
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 	printf("Size of a long: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long: %lu byte(s)\n", sizeof(alonglong));
-	printf("Size of a double: %lu bytes(s)\n", sizeof(adouble));
-	
+	printf("Size of a long long: %lu bytes(s)\n", sizeof(alonglong));	
 	return (0);
 }
