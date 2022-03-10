15c2a1eda0a0bd8698668295692ec7fd1f0385c7
Iykemanuel <mycreativeplots@gmail.com>
2022-03-10 11:14:31 -0800

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-
 int main(void)
 {
-	int aninteger;
 	char acharacter;
-	float afloat;
+	int ainteger;
 	long along;
 	long long alonglong;
+	float afloat;
 
-	printf("Size of an int: %lu byte(s)\n", sizeof(aninteger));
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long int: %lu bytes(s)\n", sizeof(alonglong));	
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
 	return (0);
 }
