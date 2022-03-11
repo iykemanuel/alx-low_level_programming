4ae7a559d124553949de4a38e5894106c0ff8b04
Iykemanuel <mycreativeplots@gmail.com>
2022-03-11 07:50:49 -0800

    positive_or_negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero or negative
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char n;
+for (n = 'a' ; n <= 'z' ; n++)
+{
+	putchar(n);
+}
+putchar('\n')
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This project is based on loops in C programming
