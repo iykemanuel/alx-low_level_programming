ec8272aff99de949f022f0ea7830b055f1091578
Iykemanuel <mycreativeplots@gmail.com>
2022-03-13 12:24:21 -0700

    print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0
+ * this program prints single digits from 0 to 9
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i" i);
+}
+printf('\n');
+return (0);
+}
