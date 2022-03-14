ddd43089a4a58f06a89141893d5c2a49f418f1cf
Iykemanuel <mycreativeplots@gmail.com>
2022-03-13 23:36:19 -0700

    print_numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+{
+	putchar(b);
+}
+putchar('\n');
+return (0);
+}
