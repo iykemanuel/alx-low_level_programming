9fe295423e04f220109a9c3b4df7e2836f8dd690
Iykemanuel <mycreativeplots@gmail.com>
2022-03-14 12:02:54 -0700

    print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point for program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char g;
+for (g = 'z'; g >= 'a'; g--)
+{
+	putchar(g);
+}
+	putchar('\n');
+	return (0);
+}
