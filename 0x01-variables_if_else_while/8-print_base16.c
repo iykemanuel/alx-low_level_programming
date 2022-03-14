e36ee21462e6d4e6051b43d4d2300a5df23ab6db
Iykemanuel <mycreativeplots@gmail.com>
2022-03-14 16:07:20 -0700

    print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,29 +5,16 @@
  * Return: Always 0
  */
 int main(void)
-
 {
-
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-
-	{
-
-			putchar(c);
-
-	}
-
-	for (c = 'a'; c <= 'f'; c++)
-
-	{
-
-			putchar(c);
-
-	}
-
-		putchar('\n');
-
-			return (0);
-
+char c;
+for (c = '0'; c <= 9; c++)
+{
+	putchar(c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+	putchar(c);
+}
+	putchar('\n');
+	return (0);
 }
