8d7330029c579d54a1f4405675285ba2056552b7
Iykemanuel <mycreativeplots@gmail.com>
2022-03-29 17:37:34 -0700

    task 0 fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _memset - check the code for Holberton School students.
- * @s: parameter for the array
- * @b: parameter for the values in the array
- * @n: parameter for the number of times to print the new values
- * Return: Always 0.
+ * _memset - Files memory with a constant byte
+ * @s: buffer array
+ * @b: constant byte
+ * @n: number of bytes of memory area to fill
+ * Return: Pointer to memory area *s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int a = 0;
+	unsigned int i;
 
-	while (a < n)
+	for (i = 0; i < n; i++)
 	{
-		s[a] = b;
-		a++;
+		s[i] = b;
 	}
-
 	return (s);
 }
