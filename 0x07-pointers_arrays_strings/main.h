ca392aef24413ac5ab55b89ce214fb96582fd1b3
Iykemanuel <mycreativeplots@gmail.com>
2022-03-29 17:35:06 -0700

    main.h fix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
@@ -12,4 +12,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#end
+#endif
