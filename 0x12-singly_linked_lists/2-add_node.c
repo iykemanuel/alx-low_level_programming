4918224a290903df189e78f2e12ba36f3220cb79
Iykemanuel <mycreativeplots@gmail.com>
2022-04-22 17:54:44 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,10 +4,11 @@
 #include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning of list_t list
+ * add_node - creates a new node at the beginning of list_t list
  * @new_head: Address of node
  * @str: New node to add
- * Return: Address of new element but NULL if it fails
+ * Return: Address of new element, NULL if it fails
+ * @head: pointer to a pointer
  */
 
 list_t *add_node(list_t **head, const char *str)
