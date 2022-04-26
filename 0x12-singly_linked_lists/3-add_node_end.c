f07b8abd28b803b046fe1847bbe1c0597311db08
Iykemanuel <mycreativeplots@gmail.com>
2022-04-26 14:38:21 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,54 +1,41 @@
-#include <string.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
-  * *add_node_end - adds a new node at the end of a linked list.
-  * @head: start of the list.
-  * @str: string.
-  * Return: the address of the new element,
-  * or null if it failed.
-  */
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head, return NULL if failed.
+ */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newNode;
+	list_t *new, *temp;
+	size_t nchar;
 
-	newNode = (list_t *)malloc(sizeof(list_t));
-	if (newNode == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	newNode->str = strdup(str);
-	newNode->len = string_length(str);
-	newNode->next = NULL;
-	/* check if list is null and make new node the head */
-	if (*head == NULL)
-		*head = newNode;
-	/* else find last node and add new node */
+
+	new->str = strdup(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new->len = nchar;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
 	else
 	{
-		list_t *lastNode = *head;
-		/* last node has to be NULL use while to check */
-		while (lastNode->next != NULL)
-		{
-			lastNode = lastNode->next;
-		}
-		/* if null is found add new node at the end */
-		lastNode->next = newNode;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
 	}
-	return (newNode);
-}
 
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(const char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
+	return (*head);
 }
